00c3c4801bf5c7ed6137dbf344f9bd8f66733c21
Adriana Stratulat-Diaconu <Adriana.Stratulat-Diaconu@windriver.com>
2020-04-10 17:45:17 +0300

    Chapter 5
    Signed-off-by: Adriana Stratulat-Diaconu <Adriana.Stratulat-Diaconu@windriver.com>

---
+++ Kernighan_and_Ritchie/Chapter 5 - Pointers and Arrays/exercise1.c
@@ -0,0 +1,62 @@
+/* 
+Exercise 5-1. As written, getint treats a + or - not followed by a digit as a valid
+representation of zero. Fix it to push such a character back on the input. 
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+char buf[100];
+int bufp = 0;
+
+// from chapter 4
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= 100)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
+
+
+int main(void)
+{
+    int n, s, v[1000], getint(int *);
+
+    for (n = 0; n < 1000 && getint(&v[n]) != EOF; n++)
+    {
+        printf("Storing in n = %d, getint %d\n", n, v[n]);
+    }
+    printf("storing in n = %d, getint %d\n", n, v[n]);
+    printf("\nThe values: ");
+    for (s = 0; s <= n; s++)
+        printf(" %d ", v[s]);
+    printf("\n");
+    return 0;
+}
+
+/* the function from page 87 */
+int getint(int *pn)
+{
+    int c, sign;
+    while (isspace(c = getch()))
+        /* skip white space */
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-'){
+        ungetch(c); /* it's not a number */
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-')
+        c = getch();
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+    *pn *= sign;
+    if (c != EOF)
+        ungetch(c);
+    return c;
+}
---
+++ Kernighan_and_Ritchie/Chapter 5 - Pointers and Arrays/exercise2.c
@@ -0,0 +1,68 @@
+/*Exercise 5-2. Write getfloat , the floating-point analog of getint . What type does
+getfloat return as its function value? */
+
+#include <stdio.h>
+#include <ctype.h>
+
+char buf[100];
+int bufp = 0;
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+void ungetch(int c)
+{
+    if (bufp >= 100)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
+
+int getfloat(float *); //return int
+
+int main(void)
+{
+    int n,j;
+    float v[1000];
+
+    for (n = 0; n < 1000 && getfloat(&v[n]) != EOF; n++)
+        ;
+    printf("\nThe values: ");
+    for (j=0; j < n; j++)
+        printf("%f ", v[j]);
+    printf("\n");
+    return 0;
+}
+
+int getfloat(float *pn)
+{
+    int c, sign;
+    float power;
+
+    while (isspace(c = getch()))
+        /* skip white space */
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-' && c != '.')
+    {
+        ungetch(c); /* it's not a number */
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-')
+        c = getch();
+    for (*pn = 0.0; isdigit(c); c = getch())
+        *pn = 10.0 * *pn + (c - '0');
+    if (c == '.') // decimals
+        c = getch();
+    for (power = 1; isdigit(c); c = getch())
+    {
+        *pn = 10 * *pn + (c - '0'); // fractional part
+        power *= 10;
+    }
+    *pn *= sign / power;
+    if (c != EOF)
+        ungetch(c);
+
+    return c;
+}
