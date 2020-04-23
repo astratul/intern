d65c5ea9b1a7768d77d746640139c0c92ff29317
Adriana Stratulat-Diaconu <Adriana.Stratulat-Diaconu@windriver.com>
2020-04-23 18:00:33 +0300

    Chapter 7
    Signed-off-by: Adriana Stratulat-Diaconu <Adriana.Stratulat-Diaconu@windriver.com>

--- Kernighan_and_Ritchie/Chapter 7 - Input and Output/exercise1.c
+++ Kernighan_and_Ritchie/Chapter 7 - Input and Output/exercise1.c
@@ -2,10 +2,30 @@
 Exercise 7-1. Write a program that converts upper case to lower or lower case to upper, depending on the name it is invoked with, as found in argv[0] .
 */
 
-// LOW - convert upper case to lower case
-// UP - convert lower case to upper case
 #include <stdio.h>
 #include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    int c;
+    if (strcmp(argv[0], "./low") == 0){ // gcc -o low exercise1.c
+        printf("Convert upper case to lower case\n");
+        while ((c = getchar()) != EOF)
+            putchar(tolower(c));
+    }
+    else if (strcmp(argv[0], "./up") == 0){ // gcc -o up exercise1.c
+        printf("Convert lower case to upper case\n");
+        while ((c = getchar()) != EOF)
+            putchar(toupper(c));
+    }
+    return 0;
+}
+
+/*
+// From arguments
+
+// LOW - convert upper case to lower case
+// UP - convert lower case to upper case
 
 int main(int argc, char *argv[]){
     int c;
@@ -21,6 +41,7 @@ int main(int argc, char *argv[]){
                 putchar(toupper(c));
         }
     else if(argc >2)
-        printf("Wrong number of arguments.\n");    
+        printf("Wrong number of arguments.\n");
     return 0;
 }
+*/
