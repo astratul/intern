751c0f662a327961f85720f1a83a00b95662d08f
Adriana Stratulat-Diaconu <Adriana.Stratulat-Diaconu@windriver.com>
2020-04-30 17:46:33 +0300

    Chapter 8
    Signed-off-by: Adriana Stratulat-Diaconu <Adriana.Stratulat-Diaconu@windriver.com>

--- Kernighan_and_Ritchie/Chapter 8 - The UNIX System Interface/exercise7.c
+++ Kernighan_and_Ritchie/Chapter 8 - The UNIX System Interface/exercise7.c
@@ -1,9 +1,11 @@
-// 8.7 Example - A Storage Allocator
+/*
+Exercise 8-7. malloc accepts a size request without checking its plausibility; free believes that the block it is asked to free contains a valid size field. Improve these routines so they make more pains with error checking.
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
 
-#define NALLOC 1024
+#define NALLOC      1024
 
 typedef long Align;
 
@@ -25,46 +27,46 @@ static Header *morecore(unsigned nu);
 void free(void *ap);
 
 void *malloc1(unsigned nbytes){
-    Header *p, *prevp;
-    Header *moreroce(unsigned);
+    Header *p,*prevp;
+    Header *morecore(unsigned);
     unsigned nunits;
-    nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
-    if ((prevp = freep) == NULL){
-        /* no free list yet */
+    if(nbytes > (unsigned)10240){
+        fprintf(stderr,"alloc: can't allocate more than %u bytes\n",(unsigned)10240);
+        return NULL;
+    }
+    // Improve these routines so they make more pains with error checking.
+    nunits = (nbytes    + sizeof(Header) -1) / sizeof(Header) +1;
+    if((prevp = freep) == NULL){
         base.s.ptr = freep = prevp = &base;
         base.s.size = 0;
     }
-
-    for (p = prevp->s.ptr;; prevp = p, p = p->s.ptr){
-        if (p->s.size >= nunits){    /* big enough */
-            if (p->s.size == nunits) /* exactly */
+    for(p = prevp->s.ptr ; ; prevp = p,p = p ->s.ptr){
+        if(p->s.size >= nunits){
+            if (p->s.size == nunits)
                 prevp->s.ptr = p->s.ptr;
             else{
-                /* allocate tail end */
-                p->s.size -= nunits;
+                p->s.size -= nunits;    
                 p += p->s.size;
                 p->s.size = nunits;
             }
             freep = prevp;
-            return (void *)(p + 1);
-        }
-        if (p == freep) /* wrapped around free list */
-            if ((p = morecore(nunits)) == NULL)
-                return NULL;
-        /* none left */
+        return (void *)(p+1);
+    }
+    if(p == freep)  /* wrapped around free list */
+        if((p = morecore(nunits)) == NULL)
+            return NULL;
     }
 }
 
 static Header *morecore(unsigned nu){
     char *cp, *sbrk(int);
     Header *up;
-    if (nu < NALLOC)
+    if (nu < NALLOC) 
         nu = NALLOC;
     cp = sbrk(nu * sizeof(Header));
-    if (cp == (char *)-1)
-        /* no space at all */
+    if (cp == (char *) -1) /* no space at all */
         return NULL;
-    up = (Header *)cp;
+    up = (Header *) cp;
     up->s.size = nu;
     free((void *)(up + 1));
     return freep;
@@ -72,33 +74,32 @@ static Header *morecore(unsigned nu){
 
 void free(void *ap){
     Header *bp, *p;
-    bp = (Header *)ap - 1;
+    bp = (Header *)ap - 1; 
     /* point to block header */
     for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
         if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
-            break; /* freed block at start or end of arena */
-    if (bp + bp->s.size == p->s.ptr){
-        /* join to upper nbr */
-        bp->s.size += p->s.ptr->s.size;
-        bp->s.ptr = p->s.ptr->s.ptr;
-    }else
+            break;  /* freed block at start or end of arena */
+    if (bp + bp->s.size == p->s.ptr) { /* join to upper nbr */
+            bp->s.size += p->s.ptr->s.size;
+            bp->s.ptr = p->s.ptr->s.ptr;
+    } else
         bp->s.ptr = p->s.ptr;
-    if (p + p->s.size == bp){
+    if (p + p->s.size == bp) { 
         /* join to lower nbr */
         p->s.size += bp->s.size;
         p->s.ptr = bp->s.ptr;
-    }else
-        p->s.ptr = bp;
+    } else 
+        p->s.ptr = bp; 
     freep = p;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     int *p = NULL;
     int i = 0;
     p = malloc1(1000);
     if (NULL == p)
         printf("returned NULL");
-    else 
+    else {
         for(i=0; i <= 100; i++) {
             printf("%08X", p[i]);
             if (i % 8 == 7) {
@@ -107,5 +108,7 @@ int main(int argc, char *argv[]){
         }
         printf("\n");
         free(p);
+    }
     return 0;
 }
+    
